fdeb8c8b6a26c639e19bd4ad33649847ff6cdd35
jijiseong <jiseong5036@gmail.com>
2022-12-26 21:55:03 +0900

    To Do App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
+function ToDo() {
+  const [value, setValue] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onClick = () => {
+    setToDoList((curList) => [value, ...curList]);
+    setValue("");
+  };
+
+  return (
+    <div>
+      <label htmlFor="todo">Todo</label>
+      <input
+        placeholder="ToDo"
+        onChange={onChange}
+        value={value}
+        name="todo"
+        type="text"
+      />
+      <button onClick={onClick}>OK</button>
+      <hr />
+
+      <ul>
+        {toDoList.map((val, index) => (
+          <li key={index}>{val}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <ToDo />
+    </div>
+  );
 }
 
 export default App;
